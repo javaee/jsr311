cff7fad7be188101a39e7168941f5237fcecc3d7
mhadley <mhadley@localhost>
2007-04-13 21:03:53 +0000

    Added CDDL header, removed resourceConfig since not required if no container SPI
    svn path=/branches/sketch1/; revision=20

--- src/jsr311-api/src/javax/ws/rs/core/EntityBody.java
+++ src/jsr311-api/src/javax/ws/rs/core/EntityBody.java
@@ -1,10 +1,19 @@
 /*
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the "License").  You may not use this file except
+ * in compliance with the License.
+ * 
+ * You can obtain a copy of the license at
+ * http://www.opensource.org/licenses/cddl1.php
+ * See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+/*
  * EntityBody.java
  *
  * Created on April 13, 2007, 4:37 PM
  *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
  */
 
 package javax.ws.rs.core;
--- src/jsr311-api/src/javax/ws/rs/core/HttpStatus.java
+++ src/jsr311-api/src/javax/ws/rs/core/HttpStatus.java
@@ -1,4 +1,15 @@
 /*
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the "License").  You may not use this file except
+ * in compliance with the License.
+ * 
+ * You can obtain a copy of the license at
+ * http://www.opensource.org/licenses/cddl1.php
+ * See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+/*
  * HttpStatus.java
  *
  * Created on April 13, 2007, 4:18 PM
