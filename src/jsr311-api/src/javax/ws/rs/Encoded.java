d2d662e4f17609b0e37b01f59a796059817351ad
sandoz <sandoz@localhost>
2007-12-13 09:25:47 +0000

    Add license and copyright
    svn path=/trunk/; revision=153

--- src/jsr311-api/src/javax/ws/rs/Encoded.java
+++ src/jsr311-api/src/javax/ws/rs/Encoded.java
@@ -1,4 +1,16 @@
 /*
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the "License").  You may not use this file except
+ * in compliance with the License.
+ * 
+ * You can obtain a copy of the license at
+ * http://www.opensource.org/licenses/cddl1.php
+ * See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/*
  * Encoded.java
  *
  * Created on June 29, 2007, 11:40 AM
