8054c2af9d2e13a5bb3d009debca1d6b8bfcbd13
mhadley <mhadley@localhost>
2008-01-03 20:15:09 +0000

    Removed Variant on evaluatePrecondition methods. The Vary header can be calculated and saved during the selectVariant method call; a single Variant doesn't contain enough information to calculate the Vary header contents.
    svn path=/trunk/; revision=159

--- src/jsr311-api/src/javax/ws/rs/core/Request.java
+++ src/jsr311-api/src/javax/ws/rs/core/Request.java
@@ -29,8 +29,9 @@ public interface Request {
     
     /**
      * Select the representation variant that best matches the request. More
-     * explicit variants are chosen ahead of less explicit ones.
-     * 
+     * explicit variants are chosen ahead of less explicit ones. A vary header
+     * is computed from the supplied list and automatically added to the 
+     * response.
      * 
      * @param variants a list of Variant that describe all of the
      * available representation variants.
@@ -43,37 +44,28 @@ public interface Request {
     /**
      * Evaluate request preconditions based on the passed in value.
      * 
-     * @param variant the representation variant to which eTag pertains, used to
-     * set the value of the Vary header in any returned Response. May be null, in
-     * which case the Vary header will not be set.
      * @param eTag an ETag for the current state of the resource
      * @return null if the preconditions are met or a Response that should be returned if the
      * preconditions are not met.
      */
-    Response evaluatePreconditions(EntityTag eTag, Variant variant);
+    Response evaluatePreconditions(EntityTag eTag);
 
     /**
      * Evaluate request preconditions based on the passed in value.
      * 
-     * @param variant the representation variant to which lastModified pertains, used to
-     * set the value of the Vary header in any returned Response. May be null, in
-     * which case the Vary header will not be set.
      * @param lastModified a date that specifies the modification date of the resource
      * @return null if the preconditions are met or a Response that should be returned if the
      * preconditions are not met.
      */
-    Response evaluatePreconditions(Date lastModified, Variant variant);
+    Response evaluatePreconditions(Date lastModified);
     
     /**
      * Evaluate request preconditions based on the passed in value.
      * 
-     * @param variant the representation variant to which lastModified and eTag pertains, used to
-     * set the value of the Vary header in any returned Response. May be null, in
-     * which case the Vary header will not be set.
      * @param lastModified a date that specifies the modification date of the resource
      * @param eTag an ETag for the current state of the resource
      * @return null if the preconditions are met or a Response that should be returned if the
      * preconditions are not met.
      */
-    Response evaluatePreconditions(Date lastModified, EntityTag eTag, Variant variant);
+    Response evaluatePreconditions(Date lastModified, EntityTag eTag);
 }
