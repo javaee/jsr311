27e42278d9c37f03cb36e0d05a3a0ffa7a996078
mhadley <mhadley@localhost>
2008-07-31 18:05:48 +0000

    Result of returning null or throwing an exception
    svn path=/trunk/; revision=445

--- src/jsr311-api/src/javax/ws/rs/ext/ExceptionMapper.java
+++ src/jsr311-api/src/javax/ws/rs/ext/ExceptionMapper.java
@@ -25,7 +25,10 @@ import javax.ws.rs.core.Response;
 public interface ExceptionMapper<E> {
 
     /**
-     * Map an exception to a {@link javax.ws.rs.core.Response}.
+     * Map an exception to a {@link javax.ws.rs.core.Response}. Returning 
+     * {@code null} results in a {@link javax.ws.rs.core.Response.Status#NO_CONTENT}
+     * response. Throwing a runtime exception results in a 
+     * {@link javax.ws.rs.core.Response.Status#INTERNAL_SERVER_ERROR} response
      * @param exception the exception to map to a response
      * @return a response mapped from the supplied exception
      */
