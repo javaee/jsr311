2a01e2279e1739a3b03186bf1440b9b3c3492400
mhadley <mhadley@localhost>
2008-08-22 19:54:35 +0000

    Add a public constructor to GenericEntity and associated test cases.
    svn path=/trunk/; revision=495

--- src/jsr311-api/src/javax/ws/rs/core/GenericEntity.java
+++ src/jsr311-api/src/javax/ws/rs/core/GenericEntity.java
@@ -28,6 +28,7 @@ package javax.ws.rs.core;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import java.lang.reflect.GenericArrayType;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import javax.ws.rs.ext.MessageBodyWriter;
@@ -42,7 +43,9 @@ import javax.ws.rs.ext.MessageBodyWriter;
  * {@link MessageBodyWriter}, this class may be used to wrap the entity and
  * capture its generic type.</p>
  * 
- * <p>Users are required to create a (typically anonymous) subclass of this 
+ * <p>There are two ways to create an instance:</p>
+ * <ol>
+ * <li>Create a (typically anonymous) subclass of this 
  * class which enables retrieval of the type information at runtime despite 
  * type erasure. For example, the following code shows how to create a
  * {@link Response} containing an entity of type {@code List<String>} whose
@@ -51,11 +54,21 @@ import javax.ws.rs.ext.MessageBodyWriter;
  * 
  * <pre>List&lt;String&gt; list = new ArrayList&lt;String&gt;();
  *GenericEntity&lt;List&lt;String&gt;&gt; entity = new GenericEntity&lt;List&lt;String&gt;&gt;(list) {};
- *Response response = Response.ok(entity);</pre>
+ *Response response = Response.ok(entity).build();</pre>
  *
  * <p>where <code>list</code> is the instance of <code>List&lt;String&gt</code>
  * that will form the response body and entity is an instance of an anonymous
- * subclass of {@code GenericEntity}.
+ * subclass of {@code GenericEntity}.</p></li>
+ * <li>Create an instance directly by supplying the generic type information
+ * with the entity. For example the following code shows how to create
+ * a response containing the result of a method invoked via reflection:
+ * <pre>Method method = ...;
+ *GenericEntity&lt;Object&gt; entity = new GenericEntity&lt;Object&gt;(
+ *    method.invoke(...), method.getGenericReturnType());
+ *Response response = Response.ok(entity).build();</pre></li>
+ * <p>The above obtains the generic type from the return type of the method,
+ * the raw type is the class of entity.<p> 
+ * </ol>
  */
 public class GenericEntity<T> {
 
@@ -79,7 +92,49 @@ public class GenericEntity<T> {
         this.type = getSuperclassTypeParameter(getClass());
         this.rawType = entity.getClass();
     }
-
+    
+    /**
+     * Create a new instance of GenericEntity, supplying the generic type
+     * information. The entity must be assignable to a variable of the
+     * supplied generic type, e.g. if {@code entity} is an instance of
+     * {@code ArrayList<String>} then {@code genericType} could
+     * be the same or a superclass of {@code ArrayList} with the same generic
+     * type like {@code List<String>}.
+     * @param entity the entity instance, must not be null
+     * @param genericType the generic type, must not be null
+     * @throws IllegalArgumentException if the entity is not assignable to
+     * a variable of the supplied generic type or if entity or genericType
+     * is null.
+     */
+    public GenericEntity(T entity, Type genericType) {
+        if (entity == null || genericType==null) {
+            throw new IllegalArgumentException("Arguments must not be null");
+        }
+        this.entity = entity;
+        this.rawType = entity.getClass();
+        checkTypeCompatibility(this.rawType, genericType);
+        this.type = genericType;
+    }
+    
+    private void checkTypeCompatibility(Class<?> c, Type t) {
+        if (t instanceof Class) {
+            Class<?> ct = (Class<?>)t;
+            if (ct.isAssignableFrom(c))
+                return;
+        } else if (t instanceof ParameterizedType) {
+            ParameterizedType pt = (ParameterizedType)t;
+            Type rt = pt.getRawType();
+            checkTypeCompatibility(c, rt);
+            return;
+        } else if (c.isArray() && (t instanceof GenericArrayType)) {
+            GenericArrayType at = (GenericArrayType)t;
+            Type rt = at.getGenericComponentType();
+            checkTypeCompatibility(c.getComponentType(), rt);
+            return;
+        }
+        throw new IllegalArgumentException("The type is incompatible with the class of the entity");
+    }
+    
     /**
      * Returns the type from super class's type parameter.
      */
--- src/jsr311-api/test/javax/ws/rs/core/GenericEntityTest.java
+++ src/jsr311-api/test/javax/ws/rs/core/GenericEntityTest.java
@@ -13,12 +13,15 @@
 package javax.ws.rs.core;
 
 import java.lang.reflect.GenericArrayType;
+import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import junit.framework.TestCase;
 
 public class GenericEntityTest extends TestCase {
@@ -93,5 +96,72 @@ public class GenericEntityTest extends TestCase {
         assertEquals(genericType, Number.class);
     }
 
-
+    @SuppressWarnings("unchecked")
+    public void testCtor() {
+        System.out.println("testCtor");
+        try {
+            // check GenericEntity(Integer, Number) works
+            Method getNumber = this.getClass().getDeclaredMethod("getNumber");
+            Type rt = getNumber.getGenericReturnType();
+            GenericEntity ge = new GenericEntity(1, rt);
+            // check GenericEntity(String, Number) fails
+            try {
+                ge = new GenericEntity("foo",rt);
+                fail("Expected IllegalArgumentException");
+            } catch (IllegalArgumentException e) {
+            }
+            // check GenericEntity(Integer[], Number[]) works
+            Method getNumbers = this.getClass().getDeclaredMethod("getNumbers");
+            rt = getNumbers.getGenericReturnType();
+            Integer ints[] = {1,2};
+            ge = new GenericEntity(ints, rt);
+            // check GenericEntity(String[], Number[]) fails
+            try {
+                String strings[] = {"foo","bar"};
+                ge = new GenericEntity(strings,rt);
+                fail("Expected IllegalArgumentException");
+            } catch (IllegalArgumentException e) {
+            }
+            // check GenericEntity(ArrayList<String>, List<String>) works
+            Method getList = this.getClass().getDeclaredMethod("getList");
+            rt = getList.getGenericReturnType();
+            ArrayList<String> als = new ArrayList<String>();
+            ge = new GenericEntity(als, rt);
+            // check GenericEntity(ArrayList<Integer>, List<String>) works
+            // note that erasure loses the generic type of the ArrayList
+            ArrayList<Integer> ali = new ArrayList<Integer>();
+            ge = new GenericEntity(ali, rt);
+            // check GenericEntity(Set<String>, List<String>) fails
+            try {
+                Set<String> ss = new HashSet<String>();
+                ge = new GenericEntity(ss, rt);
+                fail("Expected IllegalArgumentException");
+            } catch (IllegalArgumentException e) {
+            }
+            // check GenericEntity(ArrayList<String>[], List<String>[]) works
+            Method getLists = this.getClass().getDeclaredMethod("getLists");
+            rt = getLists.getGenericReturnType();
+            ArrayList<String>[] lists = new ArrayList[1];
+            ge = new GenericEntity(lists, rt);
+            // check GenericEntity(ArrayList<Integer>[], List<String>[]) works
+            // note that erasure loses the generic type of the ArrayList
+            ArrayList<Integer>[] ilists = new ArrayList[1];
+            ge = new GenericEntity(ilists, rt);
+            // check GenericEntity(Set<String>[], List<String>[]) fails
+            try {
+                Set<String>[] ss = new Set[1];
+                ge = new GenericEntity(ss, rt);
+                fail("Expected IllegalArgumentException");
+            } catch (IllegalArgumentException e) {
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Unhandled Exception");
+        }
+    }
+    
+    private Number getNumber() {return null;}
+    private Number[] getNumbers() {return null;}
+    private List<String> getList() {return null;}
+    private List<String>[] getLists() {return null;}
 }
